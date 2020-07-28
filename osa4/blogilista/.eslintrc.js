08ee7a1302024ee2763c9699f6dec97d0a8dc8c8
jliima <liimatainen.jere@gmail.com>
2020-07-28 19:20:19 +0300

    tehtavat 4.3 - 4.7 tehty

--- osa4/blogilista/.eslintrc.js
+++ osa4/blogilista/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   'env': {
     'commonjs': true,
     'es6': true,
-    'node': true
+    'node': true,
+    'jest': true
   },
   'extends': 'eslint:recommended',
   'globals': {
@@ -24,15 +25,6 @@ module.exports = {
     'semi': [
       'error',
       'never'
-    ],
-    'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 0
+    ]
   }
 }
---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,63 @@
+const blog = require("../models/blog")
+
+const dummy = blogs => 1
+
+const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+const favouriteBlog = blogs => {
+  const likesArray = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max(...likesArray)
+  return blogs.find(blog => blog.likes === maxLikes)
+
+}
+
+const mostBlogs = blogs => {
+  const authors = blogs.map(blog => blog.author)
+  let mostBlogs = 0
+
+  const bloggers = authors.map(author => {
+
+    const numberOfBlogs = blogs.reduce((sum, blog) => (
+      blog.author === author ? sum + 1 : sum
+    ), 0)
+    
+    numberOfBlogs > mostBlogs
+      ? mostBlogs = numberOfBlogs
+      : 1
+
+    return {author: author, blogs: numberOfBlogs}
+
+  })
+
+  return bloggers.find(blogger => blogger.blogs === mostBlogs)
+
+}
+
+const mostLikes = blogs => {
+  const authors = blogs.map(blog => blog.author)
+  let mostLikes = 0
+
+  const bloggers = authors.map(author => {
+
+    const numberOfLikes = blogs.reduce((sum, blog) => (
+      blog.author === author ? sum + blog.likes : sum
+    ), 0)
+    
+    numberOfLikes > mostLikes
+      ? mostLikes = numberOfLikes
+      : 1
+
+    return {author: author, likes: numberOfLikes}
+
+  })
+
+  return bloggers.find(blogger => blogger.likes === mostLikes)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes
+}
